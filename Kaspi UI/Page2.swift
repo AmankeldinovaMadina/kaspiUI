f323af8d52e4a637ec8938d5a5211ed70c77bf80
Madina Amankeldinova <madina.amankeldinova@nu.edu.kz>
2023-05-18 23:09:51 +0600

    first commit

---
+++ Kaspi UI/Page2.swift
@@ -0,0 +1,20 @@
+//
+//  Page2.swift
+//  Kaspi UI
+//
+//  Created by Madina Amankeldinova on 18.05.2023.
+//
+
+import SwiftUI
+
+struct Page2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Page2_Previews: PreviewProvider {
+    static var previews: some View {
+        Page2()
+    }
+}
---
+++ Kaspi UI/Page3.swift
@@ -0,0 +1,20 @@
+//
+//  Page3.swift
+//  Kaspi UI
+//
+//  Created by Madina Amankeldinova on 18.05.2023.
+//
+
+import SwiftUI
+
+struct Page3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Page3_Previews: PreviewProvider {
+    static var previews: some View {
+        Page3()
+    }
+}
